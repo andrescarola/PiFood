7ab0a05a9cec62e7aa4a47a2579c37edb0a18d7f
Andrés Carola <100820494+andrescarola@users.noreply.github.com>
2023-05-01 18:12:43 -0300

    retoques

--- client/src/components/navBar/navBar.jsx
+++ client/src/components/navBar/navBar.jsx
@@ -1,16 +1,18 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import style from './navBar.module.css';
 import SearchBar from '../searchBar/searchBar';
 import logo from '../img/logo.png'
 
 const NavBar = () => {
+    const location = useLocation();
+
     return (
         <nav>
             <ul className={style.navLinks}>
                 <img width={100} src={logo} alt="Logo" />
                 <li><Link to='/home'>HOME</Link></li>
                 <li><Link to='/create'>CREATE</Link></li>
-                <li><SearchBar /></li>
+                <li>{location.pathname === '/home' && <SearchBar />}</li>
             </ul>
         </nav>
     )
--- client/src/components/searchBar/searchBar.jsx
+++ client/src/components/searchBar/searchBar.jsx
@@ -1,28 +1,19 @@
 import React from 'react';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { getRecipes, getRecipesByTitle } from '../../redux/actions/actions';
+import { getRecipesByTitle } from '../../redux/actions/actions';
 import style from './searchBar.module.css';
-import { useNavigate } from 'react-router-dom';
-
-
 
 const SearchBar = () => {
     const dispatch = useDispatch();
     const [title, setTitle] = useState('');
-    const navigate = useNavigate();
 
     const inputChangeHandler = (e) => {
         setTitle(e.target.value);
     }
 
-    const searchHandler = async (e) => {
-        if (!title) {
-            await dispatch(getRecipes())
-        } else {
-            await dispatch(getRecipesByTitle(title))
-        }
-        navigate('/home');
+    const searchHandler = (e) => {
+        dispatch(getRecipesByTitle(title))
         setTitle('');
     }
 
